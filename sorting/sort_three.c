52f46526f5dea25d85e6ff0b06e582362af7c5e4
imad-touil <152611727+imad-touil@users.noreply.github.com>
2025-02-06 18:15:25 +0100

    Funny World

--- sorting/sort_three.c
+++ sorting/sort_three.c
@@ -6,39 +6,16 @@
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 21:19:51 by imatouil          #+#    #+#             */
-/*   Updated: 2025/02/05 16:41:09 by imatouil         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:11:51 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-
-
 void	sort_three(t_list **a)
 {
 	t_list	*tmp;
-	// int	second;
-	// int	third;
 
-	// first = (*a)->content;
-	// second = (*a)->next->content;
-	// third = (*a)->next->next->content;
-	// if (first > second && second > third)
-	// {
-	// 	sa(a, 1);
-	// 	rra(a, 1);
-	// }
-	// else if (first > third && third > second)
-	// 	ra(a, 1);
-	// else if (second > first && first > third)
-	// 	rra(a, 1);
-	// else if (second > third && third > first)
-	// {
-	// 	sa(a, 1);
-	// 	ra(a, 1);
-	// }
-	// else if (third > first && first > second)
-	// 	sa(a, 1);
 	tmp = get_maxindex(a);
 	if (tmp == *a)
 		ra(a, 1);
@@ -46,5 +23,4 @@ void	sort_three(t_list **a)
 		rra(a, 1);
 	if ((*a)-> content > (*a)-> next -> content)
 		sa(a, 1);
-	
 }
