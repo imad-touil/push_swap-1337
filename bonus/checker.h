975f577f134420265b6c129c120fd11601736d19
imad-touil <152611727+imad-touil@users.noreply.github.com>
2025-02-06 18:17:56 +0100

    Funny World

--- bonus/checker.h
+++ bonus/checker.h
@@ -6,7 +6,7 @@
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:04:39 by imatouil          #+#    #+#             */
-/*   Updated: 2025/02/06 16:26:09 by imatouil         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:03:32 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,6 @@
 
 # include "../get_next_line/get_next_line.h"
 # include "../push_swap.h"
-// typedef struct s_list
-// {
-// 	int				content;
-// 	int				index;
-// 	int				position;
-// 	struct s_list	*next;
-// 	struct s_list	*prev;
-// }					t_list;
 
 void	apply(t_list **a, t_list **b, char *mouve);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -42,16 +34,6 @@ void	sort_four(t_list **a, t_list **b);
 void	sort_large(t_list **a, t_list **b);
 void	sort_three(t_list **a);
 void	sort_two(t_list **a);
-
-
-// t_list	*get_maxindex(t_list **a);
-// int		is_sorted(t_list **a);
-// int		get_minindex(t_list **a);
-// void	set_position(t_list **a);
-// int		list_size(t_list **head);
-// int		is_empty(char *av);
-// void	print_error(void);
-// void	fill_stack(char **av, t_list **head);
-
+void	print_ko(void);
 
 #endif
