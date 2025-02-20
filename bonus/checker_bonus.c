f69c3032211bb8b5bb684a9e8318ad2096d31f19
imad-touil <152611727+imad-touil@users.noreply.github.com>
2025-02-20 11:02:00 +0100

    Funny World

---
+++ bonus/checker_bonus.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 15:04:13 by imatouil          #+#    #+#             */
+/*   Updated: 2025/02/20 10:57:10 by imatouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker_bonus.h"
+
+void	checker(t_list **a, t_list **b)
+{
+	char	*line;
+
+	line = get_next_line(0);
+	while (line)
+	{
+		apply(a, b, line);
+		free(line);
+		line = get_next_line(0);
+	}
+	if (!is_sorted(a) && !list_size(b))
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+}
+
+int	main(int ac, char *av[])
+{
+	t_list	*a;
+	t_list	*b;
+	int		i;
+
+	if (ac == 1)
+		return (0);
+	a = NULL;
+	b = NULL;
+	i = 0;
+	while (++i < ac)
+	{
+		if (is_empty(av[i]))
+			print_error();
+	}
+	fill_stack(av, &a);
+	checker(&a, &b); 
+	lstclear(a);
+	lstclear(b);
+	return (0);
+}
---
+++ bonus/checker_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 15:04:39 by imatouil          #+#    #+#             */
+/*   Updated: 2025/02/20 10:51:36 by imatouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_BONUS_H 
+# define CHECKER_BONUS_H
+
+# include "../get_next_line_bonus/get_next_line_bonus.h"
+# include "../push_swap.h"
+
+void	apply(t_list **a, t_list **b, char *mouve);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	push_a(t_list **ahead, t_list **bhead);
+void	push_b(t_list **ahead, t_list **bhead);
+void	rotate_a(t_list **head);
+void	rotate_b(t_list **head);
+void	rotate_a_b(t_list **a, t_list **b);
+void	rev_ra(t_list **head);
+void	rev_rb(t_list **head);
+void	rev_rr(t_list **a, t_list **b);
+void	swap_a(t_list **head);
+void	swap_b(t_list **head);
+void	swap_a_b(t_list **a, t_list **b);
+void	sort_five(t_list **a, t_list **b);
+void	sort_four(t_list **a, t_list **b);
+void	sort_large(t_list **a, t_list **b);
+void	sort_three(t_list **a);
+void	sort_two(t_list **a);
+t_list	*get_min_node(t_list **a);
+
+#endif
